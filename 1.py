5a114429c8b44825be4390e2b940ecd09f97cf5a
Eli Thomas <38369558+eliThomass@users.noreply.github.com>
2025-12-01 13:38:35 -0800

    completed puzzle 1 part 1

---
+++ 1.py
@@ -0,0 +1,28 @@
+input_array = []
+
+with open('input1.txt', 'r') as r:
+    for line in r:
+        input_array.append(line.strip())
+    
+dial = 50
+res = 0
+
+
+for rotation in input_array:
+    if rotation[0] == 'L':
+        dial -= int(rotation[1:])
+    else:
+        dial += int(rotation[1:])
+    
+    if dial > 99:
+        dial = dial % 100
+    elif dial < 0:
+        dial = dial % 100
+    
+    if dial == 0:
+        res += 1
+    
+print(res)
+
+
+
